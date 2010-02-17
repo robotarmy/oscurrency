a6b6fb6924c7b37d17028e4e81cba07c609ccf56
ken restivo <ken@restivo.org>
2010-02-16 20:29:00 -0800

    saner settings for debug/production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ config.action_controller.consider_all_requests_local = true
 config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 
-# Don't care if the mailer can't send
+# Do care if the mailer can't send
 config.action_mailer.raise_delivery_errors = true
 
 config.active_record.colorize_logging = false
