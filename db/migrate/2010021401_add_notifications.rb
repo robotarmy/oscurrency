78381004f1a5ae0520f73191bc31a8b990e552f3
Mike Travers <mt@alum.mit.edu>
2010-02-16 19:21:13 -0800

    fix ken migration

--- db/migrate/2010021401_add_notifications.rb
+++ db/migrate/2010021401_add_notifications.rb
@@ -1,11 +1,11 @@
-class AddMoreEmailNotificationPreferences < ActiveRecord::Migration
+class AddNotifications < ActiveRecord::Migration
   def self.up
-    add_column :users, :notifications, :boolean, :default => true
-    add_column :users, :active, :boolean, :default => true
+    add_column :people, :notifications, :boolean, :default => true
+    add_column :people, :active, :boolean, :default => true
   end
 
   def self.down
-    remove_column :users, :notifications
-    remove_column :users, :active
+    remove_column :people, :notifications
+    remove_column :people, :active
   end
 end
