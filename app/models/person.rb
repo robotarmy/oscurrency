df7af1afb87e44ad3e72b709395c0071b070c64e
ken restivo <ken@restivo.org>
2010-03-30 21:26:48 -0700

    refactor, and remove stray br's

--- app/models/person.rb
+++ app/models/person.rb
@@ -336,10 +336,7 @@ class Person < ActiveRecord::Base
   end
 
   def formatted_categories
-    #i'm told that this is stupid, and it'd be better to go:
-    #categories.collect{|cat| cat.long_name}.join("<br />")
-    # but i haven't tested it
-    categories.collect { |cat| cat.long_name + "<br />"}.to_s.chop.chop.chop.chop
+    categories.collect{|cat| cat.long_name}.join("<br />")
   end
 
   # from Columbia 
