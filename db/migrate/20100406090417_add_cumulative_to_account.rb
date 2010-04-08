875ed71f2eb1966c7461212cec91f9634979d23c
ken restivo <ken@restivo.org>
2010-04-07 23:20:21 -0700

    this should make the migration work on postgres

--- db/migrate/20100406090417_add_cumulative_to_account.rb
+++ db/migrate/20100406090417_add_cumulative_to_account.rb
@@ -3,6 +3,12 @@ class AddCumulativeToAccount < ActiveRecord::Migration
     add_column :accounts, :total_paid, :decimal, :precision => 8, :scale => 2, :default => 0
     add_column :accounts, :total_earned, :decimal,  :precision => 8, :scale => 2,:default => 0
     Account.reset_column_information 
+    accounts = Account.find(:all)
+    accounts.each do |a| 
+      a.total_paid = 0
+      a.total_earned = 0
+      a.save!
+    end
     exchanges = Exchange.find(:all)
     exchanges.each do |t| 
       a=t.customer.account
@@ -19,4 +25,3 @@ class AddCumulativeToAccount < ActiveRecord::Migration
     remove_column :accounts, :total_paid
   end
 end
- 
