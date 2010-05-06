d9f3a0bb53bca932c53e857e96f65c56d1423c6c
ken restivo <ken@restivo.org>
2010-05-05 20:11:30 -0700

    beginning of neighborhood browsing, addresses ticket:16

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -11,7 +11,13 @@ class Neighborhood < ActiveRecord::Base
     active_people = self.people.find(:all, :conditions => Person.conditions_for_mostly_active)
   end
 
-
+  def sub_people
+    sum = self.active_people.length
+    self.children.each {|sn| sum = sum + sn.sub_people }
+    return sum
+  end
+  
+  
   def ancestors_name
     if parent
       parent.ancestors_name + parent.name + ':'
